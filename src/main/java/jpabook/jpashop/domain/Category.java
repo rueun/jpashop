431a0140d6d59f3b057a8b08721a6bc44ecdb388
shineunjeong <dkfah1229@naver.com>
2022-09-18 00:11:31 +0900

    엔티티 설계 시 주의점(XtoOne은 fetch = LAZY)

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -25,7 +27,7 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // 셀프로 연관 관계 걸기
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
@@ -33,4 +35,9 @@ public class Category {
     private List<Category> child = new ArrayList<>();
     // END 셀프로 연관 관계 걸기
 
+    //==연관관계 편의 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,20 +19,43 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 한 명의 멤버가 주문을 여러 개 할 수 있으므로 다 대 일의 관계
+    @ManyToOne(fetch = LAZY) // 한 명의 멤버가 주문을 여러 개 할 수 있으므로 다 대 일의 관계
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItem = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL) // persist를 delivery만 해줘도 된다.
     @JoinColumn(name = "delivery_id")
-    private Delivery delivery;
+    private Delivery delivery; // 배송정보
     
     private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+/*
+    public static void main(String[] args) {
+        Member member = new Member();
+        Order order = new Order();
+
+        member.getOrders().add(order);
+        order.setMember(member);
+    }
+ */
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
